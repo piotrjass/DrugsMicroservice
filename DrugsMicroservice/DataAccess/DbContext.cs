bb817fea448bc246fef3727391d1f7cecdefc92a
Piotr Jasiulewicz <piotr.jasiulewicz@innowise.com>
2025-02-06 18:18:22 +0100

    Introduced Postgres Db Connections

---
+++ DrugsMicroservice/DataAccess/DbContext.cs
@@ -0,0 +1,30 @@
+﻿using DrugsMicroservice.BusinessLogic.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DrugsMicroservice.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+   
+        public DbSet<Drug> Drugs { get; set; }
+        public DbSet<Substance> Substances { get; set; }
+        
+        public DbSet<Disease> Diseases { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            modelBuilder.Entity<Drug>().ToTable("Drugs");
+            modelBuilder.Entity<Substance>().ToTable("Substances");
+            modelBuilder.Entity<Disease>().ToTable("Diseases");
+
+     
+        }
+    }
+}
--- DrugsMicroservice/Program.cs
+++ DrugsMicroservice/Program.cs
@@ -1,41 +1,38 @@
+using DrugsMicroservice.Application.Extensions;
+using DrugsMicroservice.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddApplicationServices(); 
+builder.Services.AddControllers();
+
+// Swagger i OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(); 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger(); 
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseRouting();
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
